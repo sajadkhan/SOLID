12fce3e2a8d5dcb32b2f0c185fd245bded0b39d5
Muhammad Sajad <sajadkhan71@yahoo.com>
2018-08-05 16:24:59 +0500

    ISP solution added

--- ProgrammingPrinciples/Interface Segregation Principle/ImageInteractionISP.swift
+++ ProgrammingPrinciples/Interface Segregation Principle/ImageInteractionISP.swift
@@ -1,5 +1,5 @@
 //
-//  ImageInteraction.swift
+//  ImageInteractionISP.swift
 //  ProgrammingPrinciples
 //
 //  Created by Sajad on 8/5/18.
@@ -7,30 +7,38 @@
 //
 
 import Foundation
+
+//This is refined version of ImageInteraction.swift, by following Interface Segregation Principle.
+
 import UIKit
 
-//A protocol to implement gesture handling on a view
-protocol Interaction {
-    //View to dectect gestures on
-    var view: UIView { get set }
-    
+//Now we have separate interfaces for all interactions
+
+protocol TapInteraction {
     //Tap Gesture
     func didTap()
-
-    //Long Press Gesture
-    func didLongPress()
-    
-    //Pinch Gesture
-    func didPinch(gesture: UIPinchGestureRecognizer)
-    
 }
 
-//We want implement zooming on our image view.
-//So we use Interection protocol
-//But as that protocol also requires to implement two other gesture so we implement them to meet the requirment but its a voilation of ISP.
-//For solution see ImageInterationISP.swift
+protocol LongPressInteraction {
+    //Long Press Gesture
+    func didLongPress()
+}
 
-class ImageZoomInteration: Interaction {
+protocol PinchInteraction {
+    //Pinch Gesture
+    func didPinch(gesture: UIPinchGestureRecognizer)
+}
+
+//Some Interaction which handles tap, long and pinch.
+/*
+class SomeInteraction: TapInteraction, LongPressInteraction, PinchInteraction {
+    // Implement all the requirements it is following
+}
+*/
+
+//ImageZoomInteraction isn't force to implement the method it is not using.
+
+class ImageZoomInteration: PinchInteraction {
     var view: UIView
     
     init(with imageView: UIImageView) {
@@ -39,12 +47,17 @@ class ImageZoomInteration: Interaction {
     }
     
     // Zooming on image using pinch gesture
-    //Interaction Protocol
-    
     private func addPinchGesture() {
         let pinch = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(gesture:)))
         view.addGestureRecognizer(pinch)
     }
+    
+    //Method to handle pinch gesture
+    @objc private func handlePinch(gesture: UIPinchGestureRecognizer) {
+        self.didPinch(gesture: gesture)
+    }
+    
+    //PinchInteraction Protocol
     
     func didPinch(gesture: UIPinchGestureRecognizer) {
         let imageView = view as! UIImageView
@@ -53,18 +66,8 @@ class ImageZoomInteration: Interaction {
             gesture.scale = 1.0
         }
     }
-    
-    //Method to handle pinch gesture
-    @objc private func handlePinch(gesture: UIPinchGestureRecognizer) {
-        self.didPinch(gesture: gesture)
-    }
-    
-    // Force to implement other gestures
-    func addTapGesture(gesture: UITapGestureRecognizer) {}
-    func didTap() {}
-    func addLongPressGesture(gesture: UILongPressGestureRecognizer) {}
-    func didLongPress() {}
 }
+
 
 
 
